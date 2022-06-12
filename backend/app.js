778792b00981678f8caabc34a97d69ab4563ea47
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-06-12 16:32:23 -0300

    Added script to start (compile client + run server). Served static compiled files in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('¡Hola mundo!')
-})
+// Servir los archivos estáticos dentro del 'public'
+app.use(express.static(path.join(__dirname, '../public')));
+
+// A las requests no identificadas con ninguna ruta de express, se les envía index.html
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Servidor Express corriendo en el puerto: ${port}`)
